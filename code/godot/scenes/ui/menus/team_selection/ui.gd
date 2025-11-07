14074270e2ee58d9ca8d82461f8acaa821e87321
Peoplesfootballexp <peoplesfootballexperience@gmail.com>
2025-11-07 04:42:50 -0500

    Added Function for loading territories

--- code/godot/scenes/ui/menus/team_selection/ui.gd
+++ code/godot/scenes/ui/menus/team_selection/ui.gd
@@ -9,6 +9,8 @@ const MAIN_SCENE: String = "res://scenes/main/main_menu.tscn";
 @onready
 var confed_selection: OptionButton = $FilterPanel/ConfedSelection;
 
+@onready
+var terr_selection : OptionButton = $FilterPanel/TerritorySelection
 
 var player_team_id: int;
 var opponent_team_id: int;
@@ -16,6 +18,7 @@ var opponent_team_id: int;
 func _ready() -> void:
 	#SaveManager.create_new_save(1, "Testing");
 	load_confederations_button()
+	load_territories_button()
 
 func load_confederations_button() -> void:
 	SaveManager.activate_save(1);
@@ -43,7 +46,32 @@ func load_confederations_button() -> void:
 			
 		index += 1;
 		
-
+func load_territories_button() -> void:
+	SaveManager.activate_save(1);
+	DBManager.init_db(SaveManager.get_active_db_path());
+	
+	# Get all confederations
+	var terrs: Array = DBManager.query_rows("SELECT * FROM Territory")
+	
+	# Example: print each confed's info
+	var index = 0;
+	for terr in terrs:
+		# Add Text Item
+		terr_selection.add_item(terr["name"], index);
+		
+		# Add Icon
+		var logo_path = "res://icon.svg" if terr["logo_path"] == null else terr["logo_path"]
+		var tex = load(logo_path)
+		if tex and tex is Texture2D:
+			terr_selection.add_item(terr["name"])      # add the label
+			terr_selection.set_item_icon(index, tex)     # assign the texture as icon
+		else:
+			push_error("Failed to load texture: %s" % terr["logo_path"])
+		
+			
+			
+		index += 1;
+	
 	
 	
 
