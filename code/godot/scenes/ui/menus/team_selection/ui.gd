4905a144dc5618822afe61a447a43c3b16e53a09
Peoplesfootballexp <peoplesfootballexperience@gmail.com>
2025-11-07 05:20:08 -0500

    Another attempt to fix Icon Width

--- code/godot/scenes/ui/menus/team_selection/ui.gd
+++ code/godot/scenes/ui/menus/team_selection/ui.gd
@@ -22,6 +22,7 @@ func _ready() -> void:
 
 func load_confederations_button() -> void:
 	var popm: PopupMenu = confed_selection.get_popup();
+	popm.add_theme_constant_override("icon_max_width", 50)
 	
 	
 	SaveManager.activate_save(1);
@@ -31,27 +32,26 @@ func load_confederations_button() -> void:
 	var confeds: Array = DBManager.query_rows("SELECT * FROM Confederation")
 	
 	# Example: print each confed's info
-	var index = 0;
 	for confed in confeds:
 		# Add Text Item
-		confed_selection.add_item(confed["name_official"], index);
+		confed_selection.add_item(confed["name_official"]);
 		
 		# Add Icon
 		var logo_path = "res://icon.svg" if confed["logo_path"] == null else confed["logo_path"]
 		var tex = load(logo_path)
 		if tex and tex is Texture2D:
-			confed_selection.add_item(confed["name_official"])      # add the label
-			confed_selection.set_item_icon(index, tex)     # assign the texture as icon
+
+			confed_selection.set_item_icon(-1, tex)     # assign the texture as icon
 		else:
 			push_error("Failed to load texture: %s" % confed["logo_path"])
 		
 			
-		popm.set_item_icon_max_width(index, 50)
-		index += 1;
+	
 		
 func load_territories_button() -> void:
 	
 	var popm: PopupMenu = confed_selection.get_popup();
+	popm.add_theme_constant_override("icon_max_width", 50)
 	SaveManager.activate_save(1);
 	DBManager.init_db(SaveManager.get_active_db_path());
 	
@@ -59,23 +59,19 @@ func load_territories_button() -> void:
 	var terrs: Array = DBManager.query_rows("SELECT * FROM Territory")
 	
 	# Example: print each confed's info
-	var index = 0;
 	for terr in terrs:
 		# Add Text Item
-		terr_selection.add_item(terr["name"], index);
+		terr_selection.add_item(terr["name"]);
 		
 		# Add Icon
 		var logo_path = "res://icon.svg" if terr["logo_path"] == null else terr["logo_path"]
 		var tex = load(logo_path)
-		if tex and tex is Texture2D:
-			terr_selection.add_item(terr["name"])      # add the label
-			terr_selection.set_item_icon(index, tex)     # assign the texture as icon
+		if tex and tex is Texture2D:     # add the label
+			terr_selection.set_item_icon(-1, tex)     # assign the texture as icon
 		else:
 			push_error("Failed to load texture: %s" % terr["logo_path"])
 		
 			
-		popm.set_item_icon_max_width(index, 50)
-		index += 1;
 	
 	
 	
